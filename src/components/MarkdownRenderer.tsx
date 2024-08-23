f91c05d8f302a53857a03b29a5bf119dfb57e7af
Sergey <seygorin@gmail.com>
2024-08-23 14:41:43 +0500

    fix: searchTerm going back

--- src/components/MarkdownRenderer.tsx
+++ src/components/MarkdownRenderer.tsx
@@ -2,6 +2,7 @@
 
 import {forwardRef} from 'react'
 import dynamic from 'next/dynamic'
+import ReactMarkdown from 'react-markdown'
 
 const MarkdownPreview = dynamic(
   () => import('@uiw/react-markdown-preview').then((mod) => mod.default),
@@ -10,10 +11,11 @@ const MarkdownPreview = dynamic(
 
 interface MarkdownRendererProps {
   content: string
+  searchTerm?: string
 }
 
 const MarkdownRenderer = forwardRef<HTMLDivElement, MarkdownRendererProps>(
-  ({content}, ref) => {
+  ({content, searchTerm}, ref) => {
     const addAnchorsToHeadings = (markdown: string) => {
       return markdown.replace(/^(#{1,6})\s+(.+)$/gm, (match, hashes, title) => {
         const anchor = title.toLowerCase().replace(/[^\w]+/g, '-')
@@ -25,7 +27,10 @@ const MarkdownRenderer = forwardRef<HTMLDivElement, MarkdownRendererProps>(
 
     return (
       <div ref={ref}>
-        <MarkdownPreview className='dark:bg-gray-800 dark:text-gray-400 p-8 pl-16' source={contentWithAnchors} />
+        <MarkdownPreview
+          className='dark:bg-gray-800 dark:text-gray-400 p-8 pl-16'
+          source={contentWithAnchors}
+        />
       </div>
     )
   }
